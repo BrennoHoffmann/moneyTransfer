772b44b837ee8bf25f3cfb709c9e4d1fb55b7ed1
Brenno Hoffmann <brenno_hoffmann@outlook.com>
2021-02-12 15:36:16 -0300

    transações entre users, bloqueando transferencia caso user seja lojista

--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Wallet;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\WalletResource;
@@ -18,22 +19,22 @@ class WalletController extends Controller
     public function add(Request $req)
     {
         $validationRules = array(
-            'payee'=>'required|min:1',
+            'payee' => 'required|min:1',
         );
         $validator = Validator::make($req->all(), $validationRules);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $validator->errors();
-        }else{
+        } else {
             $wallet = new Wallet;
-            $wallet->money = $req->money;
+            $wallet->money = $req->value;
             $wallet->payee = $req->payee;
-    
+
             $result = $wallet->save();
-    
-            if($result){
-                return ["Result" =>"Deu certo"];
-            }else{
-                return ["Result" =>"Deu ruim"];
+
+            if ($result) {
+                return ["Result" => "Deu certo"];
+            } else {
+                return ["Result" => "Deu ruim"];
             }
         }
     }
@@ -41,24 +42,45 @@ class WalletController extends Controller
     public function update(Request $req)
     {
         $validationRules = array(
-            'payee'=>'required|min:1',
+            'payee' => 'required|min:1',
+            'payer' => 'required|min:1',
         );
         $validator = Validator::make($req->all(), $validationRules);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $validator->errors();
-        }else{
-            $wallet = Wallet::find($req->id);
-            $wallet->money = $req->money;
-            $wallet->payee = $req->payee;
-    
-            $result = $wallet->save();
-            if($result){
-                return ["Result" =>"Put certo"];
+        } else {
+            $lojista = User::find($req->payer);
+            if ($lojista->user_type === 1) {
+                return ["Result" => "Lojistas nao podem fazer essa operacao"];
             }
-            else{
-                return ["Result" =>"Put ruim"];
+
+            $payer = Wallet::where('payee', $req->payer)->first();
+            $payee = Wallet::where('payee', $req->payee)->first();
+
+            if ($payer->money < $req->value) {
+                return ["Result" => "Saldo insuficiente"];
+            }
+            $payer->money = $payer->money - $req->value;
+
+            $payee->money = $payee->money + $req->value;
+            $payee->payee = $req->payee;
+
+            $authMock = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
+            $decodedMock = json_decode(file_get_contents($authMock));
+
+            if ($decodedMock->message == 'Autorizado') {
+                $payeeResult = $payee->save();
+                $payerResult = $payer->save();
+
+                if ($payeeResult && $payerResult) {
+                    $notificationMock = 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04';
+                    $decodeMock = file_get_contents($notificationMock);
+
+                    return $decodeMock;
+                }
+            } else {
+                return ["Result" => "Transação não autorizada"];
             }
         }
-
     }
 }
