f4570db7df7a6bf2eb43ab725ab9b1f208892350
Brenno Hoffmann <brenno_hoffmann@outlook.com>
2021-02-13 14:33:12 -0300

    db transaction para facilitar rollbacks

--- app/Http/Controllers/API/WalletController.php
+++ app/Http/Controllers/API/WalletController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Exception;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Wallet;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 use App\Http\Resources\WalletResource;
 
 class WalletController extends Controller
@@ -25,18 +27,19 @@ class WalletController extends Controller
         if ($validator->fails()) {
             return $validator->errors();
         } else {
-            $wallet = new Wallet;
-            $wallet->money = $req->value;
-            $wallet->payee = $req->payee;
+            try {
+                $exception = DB::transaction(function () use ($req) {
+                    $wallet = new Wallet;
+                    $wallet->money = $req->value;
+                    $wallet->payee = $req->payee;
 
-            $result = $wallet->save();
-
-            if ($result) {
+                    $wallet->save();
+                });
                 $successo = ["successo" => "Deu certo"];
                 return response()->json($successo, 200);
-            } else {
-                $error = ["error" => "Deu ruim"];
-                return response()->json($error, 401);
+            } catch (Exception $e) {
+                $successo = ["successo" => "Deu ruim"];
+                return response()->json($successo, 401);
             }
         }
     }
@@ -56,34 +59,40 @@ class WalletController extends Controller
                 $error = ["error" => "Lojistas não podem fazer essa operação"];
                 return response()->json($error, 401);
             }
+            DB::beginTransaction();
+            try {
+                $payer = Wallet::where('payee', $req->payer)->first();
+                $payee = Wallet::where('payee', $req->payee)->first();
 
-            $payer = Wallet::where('payee', $req->payer)->first();
-            $payee = Wallet::where('payee', $req->payee)->first();
-
-            if ($payer->money < $req->value) {
-                $error = ["error" => "Saldo insuficiente"];
-                return response()->json($error, 401);
-            }
-            $payer->money = $payer->money - $req->value;
-
-            $payee->money = $payee->money + $req->value;
-            $payee->payee = $req->payee;
-
-            $authMock = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
-            $decodedMock = json_decode(file_get_contents($authMock));
-
-            if ($decodedMock->message == 'Autorizado' && $payee->payee != $payer->payee) {
-                $payeeResult = $payee->save();
-                $payerResult = $payer->save();
-
-                if ($payeeResult && $payerResult) {
-                    $notificationMock = 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04';
-                    $successo = json_decode(file_get_contents($notificationMock));
-
-                    return response()->json($successo, 200);
+                if ($payer->money < $req->value) {
+                    $error = ["error" => "Saldo insuficiente"];
+                    return response()->json($error, 401);
                 }
-            } else {
-                $error = ["error" => "Transação não autorizada"];
+                $payer->money = $payer->money - $req->value;
+
+                $payee->money = $payee->money + $req->value;
+                $payee->payee = $req->payee;
+
+                $authMock = 'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6';
+                $decodedMock = json_decode(file_get_contents($authMock));
+
+                if ($decodedMock->message == 'Autorizado' && $payee->payee != $payer->payee) {
+                    $payeeResult = $payee->save();
+                    $payerResult = $payer->save();
+                    DB::commit();
+                    if ($payeeResult && $payerResult) {
+                        $notificationMock = 'https://run.mocky.io/v3/b19f7b9f-9cbf-4fc6-ad22-dc30601aec04';
+                        $successo = json_decode(file_get_contents($notificationMock));
+
+                        return response()->json($successo, 200);
+                    }
+                } else {
+                    $error = ["error" => "Transação não autorizada"];
+                    return response()->json($error, 401);
+                }
+            } catch (Exception $e) {
+                DB::rollback();
+                $error = ["error" => "Não foi possivel completar esta operação"];
                 return response()->json($error, 401);
             }
         }
